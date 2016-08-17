7d05c2c3833c9ac6996fbc8194468de9cc16ca1d
Alexandra Carpen-Amarie <carpenamarie@par.tuwien.ac.at>
2016-08-17 12:17:54 +0200

    cleanup job contents

--- pgmpi/lib/pgmpi/machsetup/machine_setup_local.py
+++ pgmpi/lib/pgmpi/machsetup/machine_setup_local.py
@@ -31,36 +31,37 @@ class PGMPIMachineConfiguratorLocal(abs_machine_setup.PGMPIAbstractMachineConfig
         
         assert(self.mpirun_path != ""), "No mpirun path specified. Please check the informations provided in the machine configuration script."
         
-        check_bench = ["if [ ! -f %s ]; then" % (bench_binary_path), 
-                        "echo \"Benchmark path incorrect: %s \"" % (bench_binary_path),
+        check_bench = ["BENCH_BINARY_PATH=\"%s\"" % (bench_binary_path),
+                       "if [ ! -f \"$BENCH_BINARY_PATH\" ]; then" , 
+                        "echo \"Benchmark path incorrect: $BENCH_BINARY_PATH \"" ,
                         "exit 1",
-                        "fi"
+                        "fi",
+                        "\n"
                         ]  
  
-        create_output_dir = ["mkdir -p %s" % (remote_output_dir),
-                             "mkdir -p %s/logs" % (remote_output_dir)
+        create_output_dir = ["OUTPUT_DIR=\"%s\"" % (remote_output_dir),
+                             "mkdir -p \"$OUTPUT_DIR\"" ,
+                             "mkdir -p \"$OUTPUT_DIR/logs\"",
+                             "\n"
                              ]
         
-        bench_call = "%s %s" % (bench_binary_path, bench_args) 
+        bench_call = "$BENCH_BINARY_PATH %s" % (bench_args) 
         mpirun_calls = []
         mpirun_args = self.__get_mpi_args(nodes, nnp)
         
         for i in range(0, nmpiruns):    
             # output files specified on the remote server
-            outname = "%s/mpi_bench_r%d.dat" % ( remote_output_dir, i)
-            outlogname = "%s/logs/mpi_bench_r%d.log" % ( remote_output_dir, i)
+            outname = "$OUTPUT_DIR/mpi_bench_r%d.dat" % (i)
+            outlogname = "$OUTPUT_DIR/logs/mpi_bench_r%d.log" % (i)
                 
             mpirun_calls += [ "echo \"Starting mpirun %d...\"" %(i),
-                             "echo \"#@nodes=%s\" > %s" % (nodes, outname),
-                             "echo \"#@nnp=%s\" >> %s" % (nnp, outname),
-                             "%s %s %s >> %s 2>> %s" % ( self.mpirun_path, mpirun_args, 
+                              "%s %s %s >> %s 2>> %s" % ( self.mpirun_path, mpirun_args, 
                                                                bench_call,
                                                                outname, outlogname),
                             "echo \"Done.\"",
                             "\n"
                             ]
         return check_bench + create_output_dir + mpirun_calls
-            
 
 
 
