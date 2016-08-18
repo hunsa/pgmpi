55927f29e62d123c3fb9ae645d93d8a0ac102d03
Alexandra Carpen-Amarie <carpenamarie@par.tuwien.ac.at>
2016-08-18 20:48:07 +0200

    added license and headers

--- pgmpi/lib/pgmpi/glconfig/glconfig.py
+++ pgmpi/lib/pgmpi/glconfig/glconfig.py
@@ -1,8 +1,25 @@
-'''
-Created on Jun 24, 2016
+#    PGMPI - Performance Guideline Verification Tool for MPI Collectives
+#
+#    Copyright 2016 Alexandra Carpen-Amarie, Sascha Hunold
+#    Research Group for Parallel Computing
+#    Faculty of Informatics
+#    Vienna University of Technology, Austria
+# 
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+# 
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-@author: carpenamarie
-'''
+
 from pgmpi.glconfig import abs_glconfig
 from pgmpi.helpers import file_helpers 
 
